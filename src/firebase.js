882b1df1650d710dfc9bf78bf6a58eb79472d7bf
Lerry Sorroche <lerrysorroche@gmail.com>
2026-05-08 23:28:17 -0300

    Security: Ocultando chaves do Firebase no .env

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBxUnc5_QRNuSb-qEF-85uXue2eD_11YgE",
-  authDomain: "bora-jogar-ba221.firebaseapp.com",
-  projectId: "bora-jogar-ba221",
-  storageBucket: "bora-jogar-ba221.firebasestorage.app",
-  messagingSenderId: "229944269493",
-  appId: "1:229944269493:web:6326ef230b84384dc33cfc",
-  measurementId: "G-4W9M3L4SEW"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
